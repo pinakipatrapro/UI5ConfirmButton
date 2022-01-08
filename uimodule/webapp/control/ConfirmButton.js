b629e49c0ebe998337386c6e45cb9f868f98f37d
pinakipatrapro <pinaki.patra.pro@gmail.com>
2022-01-08 10:51:45 +0530

    Add Custom Contol

---
+++ uimodule/webapp/control/ConfirmButton.js
@@ -0,0 +1,17 @@
+sap.ui.define(
+  ["sap/ui/core/Control"],
+  function(Control) {
+    return Control.extend("pin.org.confirmButton.control.ConfirmButton",{
+         metadata: {
+              properties: {},
+              aggregations: {},
+         },
+
+         renderer: function (oRM, oControl) {
+           
+         },
+
+         
+    });
+  }
+);
