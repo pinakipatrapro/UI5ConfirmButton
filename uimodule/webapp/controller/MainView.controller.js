4c368e13757cc780b410e4db93cba7570525c5a0
pinakipatrapro <pinaki.patra.pro@gmail.com>
2022-01-08 10:45:44 +0530

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+    "./BaseController"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller) {
+        "use strict";
+
+        return Controller.extend("pin.org.confirmButton.controller.MainView", {
+            onInit: function () {
+
+            }
+        });
+    });
